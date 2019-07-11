e6ca8140071654fcab221734e23528a23513a548
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-07-11 13:34:38 -0700

    Use arrow functions to prevent 'this' being undefined

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,15 +4,22 @@ class SearchBar extends React.Component {
 
     state = { term: ''};
 
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
 
 
     render() {
         return(
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" value={this.state.term}
+                        <input 
+                        type="text" 
+                        value={this.state.term}
                         onChange={(e) => this.setState({term: e.target.value})} />
                     </div>
                 </form>
