4bb9bcf8aab2bb26616905b9206e05ac4221bfe3
Trevor Polischuk <trevor.polischuk@gmail.com>
2019-07-11 15:53:04 -0700

    Arrow function with async syntax

--- src/components/App.js
+++ src/components/App.js
@@ -4,21 +4,23 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
 
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID 47097ea7331e3b6387a8884b0860cebd37c8e0051e60148df4ace83dae125dcc'
             }
-        }).then((response) => {
-            console.log(response.data.results);
         });
+        this.setState({ images: response.data.results });
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
